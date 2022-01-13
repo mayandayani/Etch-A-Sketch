e63a5161934bbba711c263507965e3d77abd1012
Mayan Dayani <85851994+mayandayani@users.noreply.github.com>
2022-01-12 20:10:09 -0800

    Flexbox
    Turned grid into flexbox, managed to create divs. Still stuck not complete.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
 const container = document.getElementById("container");
 
 function makeGrid() {
-    for (var i = 0; i<(8 * 8); i++) {
+    for (var i = 0; i < (169); i++) {
         var box = document.createElement('div');
         box.className = "box";
+        document.getElementById('container').appendChild(box);
     }
 }
+
+window.onload(makeGrid());
+
